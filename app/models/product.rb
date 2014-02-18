fccd74e08b3cbdb79ad0005bdf249507c4f7ab3f
shane french <ntshane@gmail.com>
2014-02-18 11:34:30 -0800

    'cssAndCustomerController'

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,8 +2,12 @@ class Product < ActiveRecord::Base
 	validates :title, :description, :image_url, presence: true
 	validates :price, numericality: {greater_than_or_equal_to: 0.01}
 	validates :title, uniqueness: true
+	validates :title, length: { minimum: 10, message: 'must be 10 characters or more.' }
 	validates :image_url, allow_blank: true, format: {
 		with: %r{\.(gif|jpg|png)\Z}i,
 		message: 'must be a URL for GIF, JPG, PNG image.'
 	}
+	def self.latest
+		Product.order(:updated_at).last
+	end
 end
